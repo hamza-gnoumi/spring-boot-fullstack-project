e694f5d552e61598fb3d481c5fdd1f9ada5950d8
hamza <hamzagnoumi@yahoo.fr>
2023-10-06 17:39:11 +0100

    fix mixed content

--- frontend/angular/src/environments/environment.prod.ts
+++ frontend/angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   api: {
-    baseUrl: 'http://spring-boot-full-project-env-1.eba-vkrgxtvq.eu-west-3.elasticbeanstalk.com',
+    baseUrl: 'spring-boot-full-project-env-1.eba-vkrgxtvq.eu-west-3.elasticbeanstalk.com',
     authUrl: '/api/v1/auth/login',
     customerUrl: '/api/v1/customers'
   }
