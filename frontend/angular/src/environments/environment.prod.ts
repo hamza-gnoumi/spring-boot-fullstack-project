74711255eb6c83bfc71b9ccde2bbe90901fbf859
hamza <hamzagnoumi@yahoo.fr>
2023-10-12 17:51:22 +0100

    update base url in prod environment.prod.ts

--- frontend/angular/src/environments/environment.prod.ts
+++ frontend/angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   api: {
-    baseUrl: 'customer-api.hamzagnouma.com',
+    baseUrl: 'https://customer-api.hamzagnouma.com',
     authUrl: '/api/v1/auth/login',
     customerUrl: '/api/v1/customers'
   }
