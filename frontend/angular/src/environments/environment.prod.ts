bd619249dcdc29a6f646d803c6326d74a7dc1a12
hamza <hamzagnoumi@yahoo.fr>
2023-10-12 17:36:41 +0100

    config angular prod environment

--- frontend/angular/src/environments/environment.prod.ts
+++ frontend/angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   api: {
-    baseUrl: 'spring-boot-full-project-env-1.eba-vkrgxtvq.eu-west-3.elasticbeanstalk.com',
+    baseUrl: 'customer-api.hamzagnouma.com',
     authUrl: '/api/v1/auth/login',
     customerUrl: '/api/v1/customers'
   }
